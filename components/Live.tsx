5be1ae9108b5b5910418975baba4bba8b8fa8a97
pardesisaksham <saksham.pardesi@zappian.com>
2024-02-04 13:07:39 +0530

    real time cursor functionality done

---
+++ components/Live.tsx
@@ -0,0 +1,80 @@
+import React, { Fragment, useCallback, useEffect, useState } from "react";
+import { useOthers, useMyPresence } from "@/liveblocks.config";
+import LiveCursor from "./Cursor/LiveCursor";
+import CursorChat from "./Cursor/CursorChat";
+import { CursorMode, CursorState } from "@/types/type";
+
+const Live = () => {
+  const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden,
+  });
+  const handleCursorMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+  const handleCursorLeave = useCallback((event: React.PointerEvent) => {
+    setCursorState({ mode: CursorMode.Hidden });
+
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+  const handleCursorDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: "",
+        });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ message: "" });
+        setCursorState({ mode: CursorMode.Hidden });
+      }
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        e.preventDefault();
+      }
+    };
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
+
+  return (
+    <Fragment>
+      <div
+        onPointerMove={handleCursorMove}
+        onPointerLeave={handleCursorLeave}
+        onPointerDown={handleCursorDown}
+        className=" h-[100vh] w-full flex justify-center items-center"
+      >
+        <h1 className="text-white text-5xl">Hello</h1>
+        {cursor && (
+          <CursorChat
+            cursor={cursor}
+            cursorState={cursorState}
+            setCursorState={setCursorState}
+            updateMyPresence={updateMyPresence}
+          />
+        )}
+        <LiveCursor others={others} />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Live;
